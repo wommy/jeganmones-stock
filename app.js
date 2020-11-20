25d13df96c59b39fb428c477a45debaf2077bcca
wommy <tom@technomad.media>
2020-11-19 19:46:03 -0500

    got the buttons to display, fixup

--- app.js
+++ app.js
@@ -1,136 +1,138 @@
-// material class: represents a new material
-class Materal {
-	constructor(title, quantity) {
-		this.title = title;
-		this.quantity = quantity;
-	}
+
+
+// what am i doing
+
+
+// create eventList
+	// fill it with events
+		// add material
+		// add quantity => 
+			// sel from all objs
+	// display it on the page
+
+// dashboard || scoreboard
+
+
+// maybe later
+	// switch dashboard from log to totals
+	// ? modal
+
+
+
+eventsArr = [
+	'newMaterial', 'addMaterialQuantity',
+	'newProduct', 'newProductQuantity',
+	'newOrder'
+]
+
+let eventButton = (name) => {
+	let btnElement = document.createElement("button")
+	btnElement.innerHTML = name
+	document.querySelector('#eventList').appendChild(btnElement)
 }
 
-// ui class: handle ui task
 
-class UI {
-	static displayMaterials() {
-		const materials = Store.getMaterials();
 
-		materials.forEach((material) => UI.addMaterialToList(material));
-	}
+// so i need to create three sections in index.html
 
-	static addMaterialToList(material) {
-		const list = document.querySelector("#material-list");
+// made the button
+const eBtn = (name) => `<button>${name}</button>`
 
-		const row = document.createElement("tr");
+const eventList = eventsArr.map( (e) => eventButton(e) )
 
-		row.innerHTML = `
-			<td>${material.title}</td>
-			<td>${material.quantity}</td>
-			<td><a href="#" class="btn delete">X</a></td>
-		`;
+// const UICreateEventList = (e) => {
+// 	return e.forEach(document.queryselector('#eventList')
+// 	.appendChild(e))
+// }
 
-		list.appendChild(row);
-	}
+// UICreateEventList(eventList);
 
-	static deleteMaterial(el) {
-		if (el.classList.contains("delete")) {
-			el.parentElement.parentElement.remove();
-		}
-	}
+// what to do
+	// write p hi to div
 
-	static showAlert(message, className) {
-		const div = document.createElement("div");
-		div.className = `alert alert-${className}`;
-		div.appendChild(document.createTextNode(message));
-		const container = document.querySelector(".container");
-		const form = document.querySelector("#material-form");
-		container.insertBefore(div, form);
 
-		// vanish in 2 seconds
-		setTimeout(() => document.querySelector(".alert").remove(), 2000);
-	}
 
-	static clearFields() {
-		document.querySelector("#title").value = "";
-		document.querySelector("#quantity").value = 0;
-	}
-}
+console.log()
+
+// lets plug it into the dom
+
+
+	// pseudo code
+// new event list
+	// from eventsArr.map
+	// for each
+	// make a button
+	// with arr
+
+// const eventlivst = (a= #eventlist, b= eventarr) => {
+// 	const list = document.queryselector("#eventlist")
+// 	list.appendchild(ebtn(addmaterial))
+// }
+
+// ^ this is map right?
+
+
+
+	// addMaterialToList(material) {
+	// 	const list = document.querySelector("#material-list");
+
+	// 	const row = document.createElement("tr");
+
+	// 	row.innerHTML = `
+	// 		<td>${material.title}</td>
+	// 		<td>${material.quantity}</td>
+	// 		<td><a href="#" class="btn delete">X</a></td>
+	// 	`;
+
+	// 	list.appendChild(row);
+	// }
+
+
+
+
+
+
+/*
+ *
+<label for="title">
+	<input type="text" id="title" class="" />
+</label>
+<label for="quantity">
+	<input type="number" id="quantity" value="0" class="" />
+</label>
+<input type="submit" value="Add Material" />
+
+
+<thead>
+	<tr>
+		<th>Title</th>
+		<th>Quantity</th>
+		<th></th>
+	</tr>
+</thead>
+<tbody id="material-list"></tbody>
+ *
+*/
+
+
+
+
 
 // store class: handles storage
-class Store {
-	static getMaterials() {
-		let materials;
-		if (localStorage.getItem("materials") === null) {
-			materials = [];
-		} else {
-			materials = JSON.parse(localStorage.getItem("materials"));
-		}
 
-		return materials;
-	}
+// event display all
 
-	static addMaterial(material) {
-		const materials = Store.getMaterials();
+// event to add a material
 
-		materials.push(material);
+// event to update material quantity
 
-		localStorage.setItem("materials", JSON.stringify(materials));
-	}
+// event to add a product
 
-	static removeMaterial(title) {
-		const materials = Store.getMaterials();
+// event to update product quantity
 
-		materials.forEach((material, index) => {
-			if (material.title === title) {
-				materials.splice(index, 1);
-			}
-		});
+// event to add an order
 
-		localStorage.setItem("materials", JSON.stringify(materials));
-	}
-}
 
-// event: display material
-document.addEventListener("DOMContentLoaded", UI.displayMaterials);
 
-// event: add material
-document.querySelector("#material-form").addEventListener("submit", (e) => {
-	// prevent actual submit
-	e.preventDefault();
 
-	// get form values
-	const title = document.querySelector("#title").value;
-	const quantity = document.querySelector("#quantity").value;
-
-	// validate
-	if (title === "" || quantity === "") {
-		UI.showAlert("Please fill in all fields", "danger");
-	} else {
-		// instantiate material
-		const material = new Materal(title, quantity);
-
-		// add material to UI
-		UI.addMaterialToList(material);
-
-		// add material to store
-		Store.addMaterial(material);
-
-		// show success message
-		UI.showAlert("Material Added", "success");
-
-		// clear fields
-		UI.clearFields();
-	}
-});
-
-// event: remove material
-document.querySelector("#material-list").addEventListener("click", (e) => {
-	// remove material from UI
-	UI.deleteMaterial(e.target);
-
-	// remove material from Store
-	Store.removeMaterial(
-		e.target.parentElement.previousElementSibling.previousElementSibling
-			.textContent
-	);
-
-	// show success message
-	UI.showAlert("Material Removed", "success");
-});
+// ui class: handles ui talks
---
+++ app0.js
@@ -0,0 +1,136 @@
+// material class: represents a new material
+class Materal {
+	constructor(title, quantity) {
+		this.title = title;
+		this.quantity = quantity;
+	}
+}
+
+// ui class: handle ui task
+
+class UI {
+	static displayMaterials() {
+		const materials = Store.getMaterials();
+
+		materials.forEach((material) => UI.addMaterialToList(material));
+	}
+
+	static addMaterialToList(material) {
+		const list = document.querySelector("#material-list");
+
+		const row = document.createElement("tr");
+
+		row.innerHTML = `
+			<td>${material.title}</td>
+			<td>${material.quantity}</td>
+			<td><a href="#" class="btn delete">X</a></td>
+		`;
+
+		list.appendChild(row);
+	}
+
+	static deleteMaterial(el) {
+		if (el.classList.contains("delete")) {
+			el.parentElement.parentElement.remove();
+		}
+	}
+
+	static showAlert(message, className) {
+		const div = document.createElement("div");
+		div.className = `alert alert-${className}`;
+		div.appendChild(document.createTextNode(message));
+		const container = document.querySelector(".container");
+		const form = document.querySelector("#material-form");
+		container.insertBefore(div, form);
+
+		// vanish in 2 seconds
+		setTimeout(() => document.querySelector(".alert").remove(), 2000);
+	}
+
+	static clearFields() {
+		document.querySelector("#title").value = "";
+		document.querySelector("#quantity").value = 0;
+	}
+}
+
+// store class: handles storage
+class Store {
+	static getMaterials() {
+		let materials;
+		if (localStorage.getItem("materials") === null) {
+			materials = [];
+		} else {
+			materials = JSON.parse(localStorage.getItem("materials"));
+		}
+
+		return materials;
+	}
+
+	static addMaterial(material) {
+		const materials = Store.getMaterials();
+
+		materials.push(material);
+
+		localStorage.setItem("materials", JSON.stringify(materials));
+	}
+
+	static removeMaterial(title) {
+		const materials = Store.getMaterials();
+
+		materials.forEach((material, index) => {
+			if (material.title === title) {
+				materials.splice(index, 1);
+			}
+		});
+
+		localStorage.setItem("materials", JSON.stringify(materials));
+	}
+}
+
+// event: display material
+document.addEventListener("DOMContentLoaded", UI.displayMaterials);
+
+// event: add material
+document.querySelector("#material-form").addEventListener("submit", (e) => {
+	// prevent actual submit
+	e.preventDefault();
+
+	// get form values
+	const title = document.querySelector("#title").value;
+	const quantity = document.querySelector("#quantity").value;
+
+	// validate
+	if (title === "" || quantity === "") {
+		UI.showAlert("Please fill in all fields", "danger");
+	} else {
+		// instantiate material
+		const material = new Materal(title, quantity);
+
+		// add material to UI
+		UI.addMaterialToList(material);
+
+		// add material to store
+		Store.addMaterial(material);
+
+		// show success message
+		UI.showAlert("Material Added", "success");
+
+		// clear fields
+		UI.clearFields();
+	}
+});
+
+// event: remove material
+document.querySelector("#material-list").addEventListener("click", (e) => {
+	// remove material from UI
+	UI.deleteMaterial(e.target);
+
+	// remove material from Store
+	Store.removeMaterial(
+		e.target.parentElement.previousElementSibling.previousElementSibling
+			.textContent
+	);
+
+	// show success message
+	UI.showAlert("Material Removed", "success");
+});
